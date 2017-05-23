0edadc994d006159767e15b5237d50c6d16654d9
Crandle, Lucas <lucas.crandle@gentex.com>
2017-05-22 22:08:53 -0400

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+/**
+ * Created by lucas.crandle on 5/22/2017.
+ */
+public class Application {
+
+    public static void main (String[] args){
+
+    }
+
+
+}
---
+++ src/main/java/Filter.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by lucas.crandle on 5/22/2017.
+ */
+public class Filter {
+    ArrayList<String> actualList;
+
+    public Filter(int size){
+        actualList = new ArrayList<String>();
+    }
+}

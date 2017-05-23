0c836f86cab24a1b5c052f07cd8325c47ae2ddb8
Crandle, Lucas <lucas.crandle@gentex.com>
2017-05-22 23:55:29 -0400

    build model. murmur hash functions are all working

--- src/main/java/Filter.java
+++ src/main/java/Filter.java
@@ -1,7 +1,10 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
-import com.sangupta.*;
+import java.util.Random;
+
 import com.sangupta.murmur.Murmur1;
+import com.sangupta.murmur.Murmur2;
+import com.sangupta.murmur.Murmur3;
 
 /**
  * Created by lucas.crandle on 5/22/2017.
@@ -9,29 +12,56 @@ import com.sangupta.murmur.Murmur1;
 public class Filter {
     ArrayList<Integer> actualList;
     boolean[] bloomFilter;
-    int size;
+    int filterSize;
     boolean useMurmur1;
     boolean useMurmur2;
     boolean useMurmur3;
+    long seed;
 
-    public Filter(int size, boolean useMurmur1, boolean useMurmur2, boolean useMurmur3){
+    public Filter(int size, boolean useMurmur1, boolean useMurmur2, boolean useMurmur3, long seed){
         actualList = new ArrayList<Integer>();
         bloomFilter = new boolean[size];
 
-        this.size = size;
+        this.filterSize = size;
 
         this.useMurmur1 = useMurmur1;
         this.useMurmur2 = useMurmur2;
         this.useMurmur3 = useMurmur3;
 
+        this.seed = seed;
+
         System.out.println("Created filter with Size: " + size);
     }
 
-    public void insert(int value){
-        ;
-        actualList.add(value);
 
-        //TODO implement hash functions
+    private void add(int value){
+        //leverage bloom filter to skip checking for multiple inserts.
+        if(!this.contains(value)){
+            if(!this.actualList.contains(value)){
+                this.actualList.add(value);
+            }
+        }else{
+            this.actualList.add(value);
+        }
+    }
+
+    public void insert(int value){
+        add(value);
+
+        byte[] bytes;
+
+        bytes = BigInteger.valueOf(value).toByteArray();
+        if(useMurmur1)
+            setFilterFlag((int)(Murmur1.hash(bytes, bytes.length, seed) % this.filterSize));
+        if(useMurmur2)
+            setFilterFlag((int)(Murmur2.hash(bytes, bytes.length, seed) % this.filterSize));
+        if(useMurmur3)
+            setFilterFlag((int)(Murmur3.hash_x86_32(bytes, bytes.length, seed) % this.filterSize));
+
+    }
+
+    private void setFilterFlag(int location){
+        bloomFilter[location] = true;
     }
 
     public boolean contains(int value){
@@ -39,15 +69,34 @@ public class Filter {
     }
 
     public boolean testFilter(int value){
-        //TODO Implement Hash Functions
-        return false;
+        byte[] bytes = BigInteger.valueOf(value).toByteArray();
+
+        boolean murmur1Result = false;
+        boolean murmur2Result = false;
+        boolean murmur3Result = false;
+
+        if(useMurmur1)
+            murmur1Result = this.bloomFilter[(int)(Murmur1.hash(bytes, bytes.length, seed) % this.filterSize)];
+        if(useMurmur2)
+            murmur2Result = this.bloomFilter[(int)(Murmur2.hash(bytes, bytes.length, seed) % this.filterSize)];
+        if(useMurmur3)
+            murmur3Result = this.bloomFilter[(int)(Murmur3.hash_x86_32(bytes, bytes.length, seed) % this.filterSize)];
+
+        return
+                ((useMurmur1 && murmur1Result)|| !useMurmur1 ) &&
+                ((useMurmur2 && murmur2Result)|| !useMurmur2) &&
+                ((useMurmur3 && murmur3Result) || !useMurmur3);
     }
 
     public void resetFilter(){
-        bloomFilter = new boolean[this.size];
+        bloomFilter = new boolean[this.filterSize];
     }
 
     public boolean[] getBloomFilter(){
         return bloomFilter;
+    }
+
+    public String filterToString(){
+        return bloomFilter.toString();
     }
 }
---
+++ src/test/java/FilterTest.java
@@ -0,0 +1,65 @@
+import com.sangupta.murmur.Murmur1;
+import com.sangupta.murmur.Murmur2;
+import com.sangupta.murmur.Murmur3;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lucas.crandle on 5/22/2017.
+ */
+public class FilterTest {
+    Filter f;
+    long randomSeed;
+
+    @Before
+    public void createSeed(){
+        Random r = new Random();
+        this.randomSeed = r.nextLong();
+    }
+    @Test
+    public void AddItemToFilter() {
+        this.f = new Filter(1000, true, true, true, randomSeed);
+        this.f.insert(127127127);
+        assertTrue(this.f.testFilter(127127127));
+        assertFalse(this.f.contains(111111111));
+    }
+
+    @Test
+    public void checkMurmur1hash(){
+        int testVal = 111111111;
+        int filterSize = 10;
+
+        byte[] bytes = BigInteger.valueOf(testVal).toByteArray();
+        f = new Filter(filterSize, true, false, false, randomSeed);
+        f.insert(testVal);
+        assertTrue(f.getBloomFilter()[(int)(Murmur1.hash(bytes, bytes.length, randomSeed) % filterSize)]);
+
+    }
+
+    @Test
+    public void checkMurmur2hash(){
+        int testVal = 111111111;
+        int filterSize = 10;
+        byte[] bytes = BigInteger.valueOf(testVal).toByteArray();
+        f = new Filter(filterSize, false, true, false, randomSeed);
+        f.insert(testVal);
+        assertTrue(f.getBloomFilter()[(int)(Murmur2.hash(bytes, bytes.length, randomSeed) % filterSize)]);
+    }
+
+    @Test
+    public void checkMurmur3hash(){
+        int testVal = 111111111;
+        int filterSize = 10;
+        byte[] bytes = BigInteger.valueOf(testVal).toByteArray();
+        f = new Filter(filterSize, false, false, true, randomSeed);
+        f.insert(testVal);
+        assertTrue(f.getBloomFilter()[(int)(Murmur3.hash_x86_32(bytes, bytes.length, randomSeed) % filterSize)]);
+
+    }
+}

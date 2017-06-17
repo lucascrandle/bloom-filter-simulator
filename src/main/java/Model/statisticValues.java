6668c5b37abd45fa521b841f718cd796a3810f97
Crandle, Lucas <lucas.crandle@gentex.com>
2017-06-16 21:25:09 -0400

    added descriptions to statistics

--- src/main/java/Model/statisticValues.java
+++ src/main/java/Model/statisticValues.java
@@ -4,10 +4,10 @@ package Model;
  * Created by lucas.crandle on 6/16/2017.
  */
 public class statisticValues {
-    public int countInSet;
-    public int falsePositives;
-    public int falsePositivePercentage;
-    public int attemptsNotInSet;
+    public int countInSet; /** Count of attempts that hit in the filter */
+    public int falsePositives; /** Count of hits that were false */
+    public int falsePositivePercentage; /** Percentage of hits that were false */
+    public int attemptsNotInSet; /** Attempts that did not hit in the filter */
 
     public statisticValues(int countInSet, int falsePositive, int attemptsNotInSet){
         this.countInSet = countInSet;
